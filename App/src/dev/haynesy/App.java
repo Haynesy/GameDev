76a21e999e6cac528f51ab57b300ae4cb59945c4
Adam Haynes <haynesy.adam@gmail.com>
2014-08-08 16:10:39 +1000

    Fixed compilation error with main signiture incorrect

---
+++ App/src/dev/haynesy/App.java
@@ -0,0 +1,20 @@
+package dev.haynesy;
+
+/**
+ * Created by NewLease on 8/08/2014.
+ */
+public class App extends  implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+
+    public static void main(String[] args){
+        App.start();
+    }
+
+    private static void start() {
+
+    }
+}
